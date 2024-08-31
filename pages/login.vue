b814c1320a7d34a6201b93678cf5311e8698aabf
Arturstriker3 <artur.danielgama@gmail.com>
2024-08-31 19:10:10 -0300

    feat: login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const payload = ref({
+    username: 'admin@admin.com',
+    password: 'admin'
+})
+
+const rulesUsername = [
+    value => !!value || 'You must enter a first name.',
+    // Adicione mais regras aqui conforme necessário
+]
+
+const rulesPassword = [
+    value => !!value || 'You must enter a password.',
+    // Adicione mais regras aqui conforme necessário
+]
+
+const passwordVisible = ref(false)
+
+const submitForm = () => {
+    console.log("LOGIN")
+}
+</script>
+
 <template>
     <div class="flex h-screen overflow-hidden gap-8">
         <div class="hidden lg:flex lg:w-1/2 items-center justify-center relative py-6">
@@ -21,8 +46,48 @@
             </div>
         </div>
 
-        <div class="w-full lg:w-1/2 bg-blue-500 flex items-center justify-center">
-            <p class="text-white">Login Page</p>
+        <div class="w-full h-full lg:w-1/2 flex flex-col justify-center items-center">
+            <div class="w-full" >
+                <div class="flex gap-2 justify-center items-center">
+                    <Icon name="ph:cat-fill" class="text-main text-5xl" />
+                    <p class="text-main font-semibold">Cat Adoption Platform</p>
+                </div>
+                <div class="flex justify-center items-center my-4" >
+                    <v-divider
+                        :thickness="2"
+                        class="border-opacity-25 max-w-96"
+                        color="error"
+                    ></v-divider>
+                </div>
+                <v-sheet class="mx-auto"  maxWidth="400">
+                    <v-form @submit.prevent>
+                        <h3 class="text-black font-semibold text-2xl mb-4" >Login</h3>
+
+                        <span class="text-main font-semibold" >Username</span>
+                        <v-text-field
+                            v-model="payload.username"
+                            :rules="rulesUsername"
+                            placeholder="Enter your username"
+                            variant="outlined"
+                            class="mt-2"
+                        ></v-text-field>
+
+                        <span class="text-main font-semibold" >Password</span>
+                        <v-text-field
+                            v-model="payload.password"
+                            :rules="rulesPassword"
+                            :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                            :type="passwordVisible ? 'text' : 'password'"
+                            placeholder="Enter your password"
+                            variant="outlined"
+                            class="mt-2"
+                            @click:append-inner="passwordVisible = !passwordVisible"
+                        ></v-text-field>
+                        
+                        <v-btn class="mt-2 bg-main" size="large" type="submit" block color="main">Login</v-btn>
+                    </v-form>
+                </v-sheet>
+            </div>
         </div>
     </div>
 </template>
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -5,6 +5,21 @@ import { createVuetify } from 'vuetify'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
+    theme: {
+      themes: {
+        light: {
+          colors: {
+            main: '#4E2096',
+            textPrimary: '#26203B',
+            textSecundary: '#4D4E4E',
+            stroke: '#DFE4E8',
+            placeholder: '#B2B2B2',
+            danger: '#E03131',
+            success: '#08D58B',
+          },
+        },
+      },
+    },
   })
   app.vueApp.use(vuetify)
 })
